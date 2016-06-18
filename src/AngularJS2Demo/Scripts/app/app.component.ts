b2eae53232f4b1d40788e0667b60ebd4e3bd783c
Thanh Truong <thanh.v.truong@gmail.com>
2016-06-18 15:45:35 -0700

    simple angular js 2 page running

---
+++ src/AngularJS2Demo/Scripts/app/app.component.ts
@@ -0,0 +1,8 @@
+﻿import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'angularjs2demo',
+  template: '<h1>AngularJS 2 Demo</h1><div>Hello ASP.NET Core! Greetings from AngularJS 2.</div>'
+})
+
+export class AppComponent { }  
---
+++ src/AngularJS2Demo/wwwroot/app/boot.js
@@ -0,0 +1,2 @@
+System.register(["angular2/platform/browser","./app.component"],function(a,b){"use strict";var c,d;b&&b.id;return{setters:[function(a){c=a},function(a){d=a}],execute:function(){c.bootstrap(d.AppComponent)}}});
+//# sourceMappingURL=boot.js.map
